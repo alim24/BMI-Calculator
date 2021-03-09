458384a150aa387984042741a37fec97df2c3ac9
DESKTOP-LMFN1IB\CodeFirst <fahmialim24@gmail.com>
2021-03-09 15:34:29 +0700

    balbal

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -7,7 +7,7 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demijijio',
+      title: 'Flutter Demijijiao',
       theme: ThemeData(),
       home: HomePage(),
       debugShowCheckedModeBanner: true,
